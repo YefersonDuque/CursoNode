abababe0fe0adda39cce3863105eb1d6ca34711a
YefersonDuque <duqueyeferson@gmail.com>
2025-06-04 11:12:32 -0500

    callbacks

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,3 +1,14 @@
-const {emailTemplate} = require('./js-foundation/01-template')
+// const {emailTemplate} = require('./js-foundation/01-template')
 
-console.log(emailTemplate);
+// require('./js-foundation/02-destructuring')
+
+const { getUserById } = require('./js-foundation/03-callbacks')
+
+const id = 2;
+
+getUserById(id, function (error, user) {
+    if (error) {
+        throw new Error( error);
+    }
+    console.log(user);
+});
---
+++ 02-bases/src/js-foundation/02-destructuring.js
@@ -0,0 +1,11 @@
+// console.log(process.env);
+
+const {USERNAME,USERPROFILE } =process.env;
+
+// console.table({USERNAME, USERPROFILE});
+
+const characters = ['Flash','Superman','Batman'];
+
+const [_,__,batman] = characters;
+
+console.log(__)
