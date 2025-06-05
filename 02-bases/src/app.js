66e1f582556714f25773d529fdb5f75eecfcfd43
YefersonDuque <duqueyeferson@gmail.com>
2025-06-04 21:29:52 -0500

    logs

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,8 +1,12 @@
 const getPokemonById = require('./js-foundation/06-promises')
+const { buildLogger } = require('./plugins')
 
+const logger = buildLogger('app.js')
+
+logger.log('Hola mundo');
+logger.error('Hola mundo');
 
 getPokemonById(4)
-.then((pokemon)=>console.log({pokemon}))
-.catch((err)=> console.log({err}))
+.then((pokemon)=>logger.log({pokemon}))
+.catch((err)=> logger.error({err}))
 .finally(()=>console.log('Final'))
-
--- 02-bases/src/plugins/index.js
+++ 02-bases/src/plugins/index.js
@@ -2,9 +2,12 @@ const { getUUID } = require('../plugins/get-id.plugin');
 const { getAge } = require('../plugins/get-age.plugin');
 const {http} = require('./http-client.plugin')
 
+const buildLogger =  require('./logger.plugin')
+
 module.exports = {
     getAge,
     getUUID,
-    http
+    http,
+    buildLogger
 
 }
