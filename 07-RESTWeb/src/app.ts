6b65894e35573ba801397c378da37fdfcda25794
Yeferson <duqueyeferson@gmail.com>
2025-06-16 12:04:40 -0500

    fin sesion 13 - inicio de 14

--- 07-RESTWeb/src/app.ts
+++ 07-RESTWeb/src/app.ts
@@ -1,1 +1,17 @@
-console.log('hola')
+// src/app.ts
+import { envs } from "./config/envs";
+import { Server } from "./presentation/server";
+
+(async()=> {
+    main();
+})();
+
+function main(){
+    // Pasamos el puerto opcional
+    const server = new Server({ 
+        port: envs.PORT,
+        public_path: envs.PUBLIC_PATH
+     });
+    server.start();
+}
+
