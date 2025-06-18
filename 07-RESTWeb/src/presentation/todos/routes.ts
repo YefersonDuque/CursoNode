291d4437c405acc4f55626c6476fd61cfdfe8fb6
Yeferson <duqueyeferson@gmail.com>
2025-06-18 09:16:41 -0500

    fin sesion 15 - inicio 16

--- 07-RESTWeb/src/presentation/todos/routes.ts
+++ 07-RESTWeb/src/presentation/todos/routes.ts
@@ -9,8 +9,8 @@ export class TodosRoutes {
     // Rutas CRUD
     router.get('/', (req, res) => todoController.getTodos(req, res));
     router.get('/:id', (req, res) => todoController.getTodosById(req, res));
-    router.post('/', (req, res) => todoController.createTodo(req, res));
-    router.put('/:id', (req, res) => todoController.updateTodo(req, res));
+    // router.post('/', (req, res) => todoController.createTodo(req, res));
+    // router.put('/:id', (req, res) => todoController.updateTodo(req, res));
     router.delete('/:id', (req, res) => todoController.deleteTodo(req, res));
 
     return router;
