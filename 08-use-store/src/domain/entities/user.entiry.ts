a9c87d083ae5ff530dbf6c55f049be63baededfb
Yeferson <duqueyeferson@gmail.com>
2025-06-18 17:15:06 -0500

    user entity

--- 08-use-store/src/domain/entities/user.entiry.ts
+++ 08-use-store/src/domain/entities/user.entiry.ts
@@ -13,21 +13,24 @@ export class UserEntity {
 
     ) { }
 
-    static fromObject(object: {[key:string]:any}){
-        const {id, _id, name, email, emailValidated, password, role, img} = object;
+    static fromObject(object: { [key: string]: any }) {
+        const { id, _id, name, email, emailValidated, password, role, img } = object;
 
-        if(!id && !_id){
+        if (!id && !_id) {
             throw CustomError.badReques('Missing id');
         }
 
-        if(!name) throw CustomError.badReques('Missing name');
-        
-        if(!email) throw CustomError.badReques('Missing email');
-        if(emailValidated) throw CustomError.badReques('Missing emailValidated');
+        if (!name) throw CustomError.badReques('Missing name');
 
-        if(!password) throw CustomError.badReques('Missing password');
+        if (!email) throw CustomError.badReques('Missing email');
 
+        if (emailValidated) throw CustomError.badReques('Missing emailValidated');
 
+        if (!password) throw CustomError.badReques('Missing password');
+
+        if (!role) throw CustomError.badReques('Missing role');
+
+        return new UserEntity(id || _id, name, email, emailValidated, password, role, img);
 
     }
 }
